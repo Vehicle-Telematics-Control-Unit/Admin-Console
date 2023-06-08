959d7407a30b76f70cfb9e6506b00b62ed5e1e57
mhwahdan <moustafawahdan55@gmail.com>
2023-06-09 01:16:26 +0300

    Added more files

---
+++ Data/BackEndApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Admin_Console.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Admin_Console.Data
+{
+    public class BackEndApplicationDbContext : IdentityDbContext<BackEndApplicationUser, IdentityRole, string>
+    {
+        public BackEndApplicationDbContext(DbContextOptions<BackEndApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("TcuServerConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
